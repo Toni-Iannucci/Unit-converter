1cfd93476eba3697502b55ffb6b8f432d0b37b02
Toni Iannucci <40055475+ToniIann@users.noreply.github.com>
2025-04-05 22:25:51 +0200

    Remplacement de tofixed par Math.number

--- script.js
+++ script.js
@@ -7,14 +7,21 @@ const input = document.getElementById("input");
 convertButton.addEventListener("click", () => {
 	const value = input.value;
 
-	const metersToFeet = (value * 3.281).toFixed(2);
-	const feetToMeters = (value / 3.281).toFixed(2);
+	if (isNaN(value) || value < 0) {
+		alert("Veuillez entrer un nombre valide et positif.");
+		return;
+	}
 
-	const litersToGallons = (value * 0.264).toFixed(2);
-	const gallonsToLiters = (value / 0.264).toFixed(2);
+	// const metersToFeet = (value * 3.281).toFixed(2);
+	const metersToFeet = Math.round(value * 3.281 * 100) / 100;
 
-	const kilogramsToPounds = (value * 2.204).toFixed(2);
-	const poundsToKilograms = (value / 2.204).toFixed(2);
+	const feetToMeters = Math.round((value / 3.281) * 100) / 100;
+
+	const litersToGallons = Math.round(value * 0.264 * 100) / 100;
+	const gallonsToLiters = Math.round((value / 0.264) * 100) / 100;
+
+	const kilogramsToPounds = Math.round(value * 2.204 * 100) / 100;
+	const poundsToKilograms = Math.round((value / 2.204) * 100) / 100;
 	document.getElementById("length-result").textContent = `${value} meters = ${metersToFeet} feet | ${value} feet = ${feetToMeters} meters`;
 	document.getElementById("volume-result").textContent = `${value} liters = ${litersToGallons} gallons | ${value} gallons = ${gallonsToLiters} liters`;
 	document.getElementById("mass-result").textContent = `${value} kilograms = ${kilogramsToPounds} pounds | ${value} pounds = ${poundsToKilograms} kilograms`;
