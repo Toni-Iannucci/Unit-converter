3ea45f815ef3e41ab4f5c75484d1e97644941da1
Toni Iannucci <40055475+ToniIann@users.noreply.github.com>
2025-04-05 21:11:06 +0200

    Fin du projet

--- script.js
+++ script.js
@@ -2,11 +2,20 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound*/
 const convertButton = document.getElementById("convert-btn");
-const lengthInput = document.getElementById("length-input");
+const input = document.getElementById("input");
 
 convertButton.addEventListener("click", () => {
-  const value = Number(lengthInput.value);
-  const metersToFeet = Number(value * 3.281).toFixed(2);
-  const litersToGallons = Number(value * 0.264).toFixed(2);
-  const kilogramsToPounds = Number(value * 2.204).toFixed(2);
+	const value = input.value;
+
+	const metersToFeet = (value * 3.281).toFixed(2);
+	const feetToMeters = (value / 3.281).toFixed(2);
+
+	const litersToGallons = (value * 0.264).toFixed(2);
+	const gallonsToLiters = (value / 0.264).toFixed(2);
+
+	const kilogramsToPounds = (value * 2.204).toFixed(2);
+	const poundsToKilograms = (value / 2.204).toFixed(2);
+	document.getElementById("length-result").textContent = `${value} meters = ${metersToFeet} feet | ${value} feet = ${feetToMeters} meters`;
+	document.getElementById("volume-result").textContent = `${value} liters = ${litersToGallons} gallons | ${value} gallons = ${gallonsToLiters} liters`;
+	document.getElementById("mass-result").textContent = `${value} kilograms = ${kilogramsToPounds} pounds | ${value} pounds = ${poundsToKilograms} kilograms`;
 });
